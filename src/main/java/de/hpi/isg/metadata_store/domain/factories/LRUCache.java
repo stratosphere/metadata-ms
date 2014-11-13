5ebc2848877e269eedefe9b5dff123055bf2c380
Fabian Tschirschnitz <fatschi@googlemail.com>
2014-11-13 17:51:42 +0100

    feat(metadatastore): added lru caching for rdbms objects
        * added lru cache for recently queried col, table, and schemas as well as collections
    for of them, still kind of slow, wip

---
+++ src/main/java/de/hpi/isg/metadata_store/domain/factories/LRUCache.java
@@ -0,0 +1,22 @@
+package de.hpi.isg.metadata_store.domain.factories;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class LRUCache<K, V> extends LinkedHashMap<K, V> {
+    /**
+     * 
+     */
+    private static final long serialVersionUID = -4065480724108252213L;
+    private final int maxEntries;
+
+    public LRUCache(final int maxEntries) {
+        super(maxEntries + 1, 1.0f, true);
+        this.maxEntries = maxEntries;
+    }
+
+    @Override
+    protected boolean removeEldestEntry(final Map.Entry<K, V> eldest) {
+        return super.size() > maxEntries;
+    }
+}
