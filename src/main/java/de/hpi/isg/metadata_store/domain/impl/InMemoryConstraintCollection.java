641234c5145b13e48b886b351621a925982c4560
Sebastian Kruse <sebastian.kruse@hpi.de>
2015-02-20 17:30:31 +0100

    feat(metadata-store): add a dummy constraint collection suitable for dry-runs

---
+++ src/main/java/de/hpi/isg/metadata_store/domain/impl/InMemoryConstraintCollection.java
@@ -0,0 +1,69 @@
+package de.hpi.isg.metadata_store.domain.impl;
+
+import de.hpi.isg.metadata_store.domain.Constraint;
+import de.hpi.isg.metadata_store.domain.ConstraintCollection;
+import de.hpi.isg.metadata_store.domain.MetadataStore;
+import de.hpi.isg.metadata_store.domain.Target;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedList;
+
+/**
+ * This constraint collection only saves constraints in-memory. It is suitable for dry-runs.
+ *
+ * @author sebastian.kruse
+ * @since 20.02.2015
+ */
+public class InMemoryConstraintCollection implements ConstraintCollection {
+
+    private final Collection<Target> scope;
+
+    private final MetadataStore metadataStore;
+
+    public InMemoryConstraintCollection(MetadataStore metadataStore, Target scope) {
+        this(metadataStore, Collections.singleton(scope));
+    }
+    public InMemoryConstraintCollection(MetadataStore metadataStore, Collection<Target> scope) {
+        this.metadataStore = metadataStore;
+        this.scope = scope;
+    }
+
+    private final Collection<Constraint> constraints = new LinkedList<>();
+    private String description = "in-memory metadata store";
+
+    @Override
+    public Collection<Constraint> getConstraints() {
+        return this.constraints;
+    }
+
+    @Override
+    public Collection<Target> getScope() {
+        return this.scope;
+    }
+
+    @Override
+    public void add(Constraint constraint) {
+        this.constraints.add(constraint);
+    }
+
+    @Override
+    public MetadataStore getMetadataStore() {
+        return this.metadataStore;
+    }
+
+    @Override
+    public String getDescription() {
+        return this.description;
+    }
+
+    @Override
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public int getId() {
+        return 0;
+    }
+}
