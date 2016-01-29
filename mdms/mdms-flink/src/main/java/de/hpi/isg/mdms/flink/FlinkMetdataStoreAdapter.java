ee8222f0350c19d85ca3aebbabb9a1049e8e4b71
Sebastian Kruse <sebastian.kruse@hpi.de>
2016-01-29 23:01:56 +0300

    refactor(FlinkMetadataStoreAdapter): collect dependecies without spawning new threads

--- mdms/mdms-flink/src/main/java/de/hpi/isg/mdms/flink/FlinkMetdataStoreAdapter.java
+++ mdms/mdms-flink/src/main/java/de/hpi/isg/mdms/flink/FlinkMetdataStoreAdapter.java
@@ -1,42 +1,51 @@
 package de.hpi.isg.mdms.flink;
 
+import de.hpi.isg.mdms.flink.serializer.AbstractFlinkSerializer;
+import de.hpi.isg.mdms.model.MetadataStore;
+import de.hpi.isg.mdms.model.constraints.ConstraintCollection;
+import org.apache.flink.api.java.DataSet;
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.io.RemoteCollectorImpl;
+import org.apache.flink.api.java.tuple.Tuple;
+
 import java.io.Serializable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-import org.apache.flink.api.java.DataSet;
-import org.apache.flink.api.java.ExecutionEnvironment;
-import org.apache.flink.api.java.io.RemoteCollectorConsumer;
-import org.apache.flink.api.java.io.RemoteCollectorImpl;
-import org.apache.flink.api.java.tuple.Tuple;
 
-import de.hpi.isg.mdms.flink.serializer.AbstractFlinkSerializer;
-import de.hpi.isg.mdms.model.MetadataStore;
-import de.hpi.isg.mdms.model.constraints.ConstraintCollection;
+public class FlinkMetdataStoreAdapter implements Serializable {
 
+    private static final long serialVersionUID = 1L;
 
-public class FlinkMetdataStoreAdapter implements Serializable{
-	
-	private static final long serialVersionUID = 1L;
+    /**
+     * @deprecated Don't use this method unless thread leaks are acceptable (e.g., in short-lived apps).
+     */
+    public static <T extends Tuple> void saveAsync(DataSet<T> constraints, final ConstraintCollection constraintCollection, final AbstractFlinkSerializer flinkSerializer) {
 
-	public static <T extends Tuple> void save(DataSet<T> constraints, final ConstraintCollection constraintCollection, final AbstractFlinkSerializer flinkSerializer){
-		
-		final ExecutorService executorService = Executors.newSingleThreadExecutor();
-		RemoteCollectorImpl.collectLocal(constraints, new RemoteCollectorConsumer<T>() {
-			
-			            @Override
-			            public void collect(T tuple) {
-			                executorService.execute(flinkSerializer.getAddRunnable(tuple, constraintCollection));
-			            }
-			        });
-		}
+        final ExecutorService executorService = Executors.newSingleThreadExecutor();
+        RemoteCollectorImpl.collectLocal(constraints, tuple -> {
+            executorService.execute(flinkSerializer.getAddRunnable(tuple, constraintCollection));
+        });
+    }
 
-	public static <T extends Tuple> DataSet<T> getConstraintsFromCollection(
-			ExecutionEnvironment executionEnvironment,
-			MetadataStore metadataStore,
-			ConstraintCollection datasourceCollection, AbstractFlinkSerializer flinkSerializer) {
+    /**
+     * @deprecated Don't use this method unless thread leaks are acceptable (e.g., in short-lived apps).
+     */
+    public static <T extends Tuple> void save(DataSet<T> constraints, final ConstraintCollection constraintCollection, final AbstractFlinkSerializer flinkSerializer) {
 
-		return flinkSerializer.getConstraintsFromCollection(executionEnvironment, metadataStore, datasourceCollection);
-	}
+        RemoteCollectorImpl.collectLocal(constraints, tuple -> {
+            synchronized (flinkSerializer) {
+                flinkSerializer.getAddRunnable(tuple, constraintCollection).run();;
+            }
+        });
+    }
+
+    public static <T extends Tuple> DataSet<T> getConstraintsFromCollection(
+            ExecutionEnvironment executionEnvironment,
+            MetadataStore metadataStore,
+            ConstraintCollection datasourceCollection, AbstractFlinkSerializer flinkSerializer) {
+
+        return flinkSerializer.getConstraintsFromCollection(executionEnvironment, metadataStore, datasourceCollection);
+    }
 
 }
