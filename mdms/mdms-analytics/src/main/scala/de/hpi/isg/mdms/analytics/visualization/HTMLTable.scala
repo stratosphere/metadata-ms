e6dcbfccd2301404d383d3475966d159bb5a7623
lawrencebenson <lawrence.benson@student.hpi.de>
2016-05-26 16:00:36 +0200

    Add context to rendering through RenderingConfig

--- mdms/mdms-analytics/src/main/scala/de/hpi/isg/mdms/analytics/visualization/HTMLTable.scala
+++ mdms/mdms-analytics/src/main/scala/de/hpi/isg/mdms/analytics/visualization/HTMLTable.scala
@@ -2,16 +2,23 @@ package de.hpi.isg.mdms.analytics.visualization
 
 import de.hpi.isg.mdms.analytics.ConstraintImplicits.ConstraintCollectionQueryObject
 import de.hpi.isg.mdms.analytics.{GroupedConstraintCollection, JoinedConstraintCollection}
+import de.hpi.isg.mdms.domain.constraints.{InclusionDependency, UniqueColumnCombination}
+import de.hpi.isg.mdms.domain.util.DependencyPrettyPrinter
 import de.hpi.isg.mdms.model.constraints.{Constraint, ConstraintCollection}
-import scala.reflect.runtime.universe.{typeOf, TypeTag}
 
+import scala.reflect.runtime.universe.{TypeTag, typeOf}
 import scala.xml.Elem
 
 object HTMLTable {
 
-  // TODO: Handle empty lists
+  var renderingConf: RenderingConf = UninitializedRenderingConfig
+
   val baseTable = <table style="border: 1px solid black; border-collapse: collapse;"/>
   val emptyTable = createEmptyTable()
+
+  def initialize(renderingConfig: RenderingConf): Unit = {
+    renderingConf = renderingConfig
+  }
 
   def fromJoin(joined: JoinedConstraintCollection[_ <: Constraint, _ <: Constraint]): Elem = {
     fromCollectionTuple(joined.selectAll())
@@ -22,7 +29,7 @@ object HTMLTable {
       group.map { case (a, b) => List(k, a, b) }
     }
 
-    val head = List(s"Key", genericConstraintName[A], genericConstraintName[B])
+    val head = List("Key", genericConstraintName[A], genericConstraintName[B])
     newTable(data, Some(head))
   }
 
@@ -32,14 +39,13 @@ object HTMLTable {
 
   def fromCollection[A <: Constraint: TypeTag](collection: Iterable[A]): Elem = {
     val data = collection.map(List(_))
-    val head = List(genericConstraintName[A]) //collection.head.getClass.getSimpleName
+    val head = List(genericConstraintName[A])
     newTable(data, Some(head))
   }
 
   def fromCollectionTuple[A <: Constraint: TypeTag, B <: Constraint: TypeTag](collection: Iterable[(A, B)]): Elem = {
     val data = collection.map { case (a, b) => List(a, b) }
     val head = List(genericConstraintName[A], genericConstraintName[B])
-    //List(collection.head._1.getClass.getSimpleName, collection.head._2.getClass.getSimpleName)
     newTable(data, Some(head))
   }
 
@@ -57,12 +63,20 @@ object HTMLTable {
   }
 
   private def createTableRows(rows: Iterable[Iterable[Any]]): Iterable[Elem] = {
+    // TODO: DependencyPrettyPrinter for Constraints. Get Metadatastore into context.
+    renderingConf.checkInitialization
+    val dpp = new DependencyPrettyPrinter(renderingConf.mds)
     rows.map(row =>
       <tr>
         {
         row.map(cell =>
-          <td style="border: 1px solid black; text-align: left;">
-            { cell }
+          <td style="border: 1px solid black; text-align: left;"> {
+            cell match {
+              case ind: InclusionDependency => dpp.prettyPrint(ind)
+              case ucc: UniqueColumnCombination => dpp.prettyPrint(ucc)
+              case _ => cell
+            }
+            }
           </td>)
         }
       </tr>)
